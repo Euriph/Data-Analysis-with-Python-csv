ed02b075343e6c043a37c61113a568d5e22b4cf9
ilyas <ilyascoskun200@gmail.com>
2024-05-09 20:26:19 +0300

    rearranged names and files

--- Api/config.py
+++
@@ -1,2 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123578951@localhost/dataanalysisproject'
--- Api/main.py
+++
@@ -1,5 +0,0 @@
-from app import app
-from resources import *
-
-if __name__ == '__main__':
-    app.run(debug=True)
